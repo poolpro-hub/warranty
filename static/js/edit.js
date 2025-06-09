15fbe9d8ce9b2bd0102ce2a5ac54809535188e94
Coopz <coopzpoolpro@gmail.com>
2025-06-09 13:35:35 +1000

    Update edit.js

--- static/js/edit.js
+++ static/js/edit.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   }
 
   const { data, error } = await supabase
-    .from('warrantyrequests')
+    .from('tblwarranty')
     .select('*')
     .ilike('claimnumber', claimNumber)
     .single();
