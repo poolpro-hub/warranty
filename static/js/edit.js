2272c8a1cc10665970853696c1d3353191b98949
Coopz <coopzpoolpro@gmail.com>
2025-06-09 14:14:06 +1000

    Update edit.js

--- static/js/edit.js
+++ static/js/edit.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     return;
   }
 
-  const nonEditableFields = ['id', 'claimnumber', 'submissiondate'];
+  const nonEditableFields = ['id', 'claimnumber', 'submissiondate', 'created_at'];
   const editableFields = Object.keys(data).filter(f => !nonEditableFields.includes(f));
 
   editableFields.forEach(field => {
