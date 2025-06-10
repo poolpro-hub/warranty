035abab95fb9f59077c34cf194d4c9d25d32c07c
Coopz <coopzpoolpro@gmail.com>
2025-06-10 15:46:31 +1000

    Update dashboard.js

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,8 +1,10 @@
 // js/dashboard.js
 document.addEventListener('DOMContentLoaded', async () => {
   const statuses = ['New', 'Viewed', 'Progress', 'Rejected', 'Complete'];
+  const statusesInfield = ['New', 'Progress', 'Complete'];
   const bkgpanel = ['bg-primary', 'bg-success', 'bg-warning', 'bg-danger', 'bg-dark'];
   const panelContainer = document.getElementById('status-panels');
+  const panelContainerInfield = document.getElementById('status-panels-infield');
   let bkgcount = 0;
 
   for (const status of statuses) {
@@ -35,6 +37,46 @@ document.addEventListener('DOMContentLoaded', async () => {
       `;
 
       panelContainer.appendChild(col);
+      // increment bkg count to move through the array
+      bkgcount++;
+    } catch (err) {
+      console.error(`Unexpected error for ${status}:`, err);
+    }
+  }
+
+
+  
+  for (const statusInfield of statusesInfield) {
+    try {
+      const { data, error } = await supabase
+        .from('tblwarranty')
+        .select('claimnumber')
+        .eq('status', statusInfield)
+        .eq('infield', 'Yes');
+
+      if (error) {
+        console.error(`Error fetching count for ${statusInfield}:`, error.message);
+        continue;
+      }
+
+      const count = data ? data.length : 0;
+
+      const col = document.createElement('div');
+      col.className = 'col-md-4 mb-4';
+
+      col.innerHTML = `
+        <div class="card text-white ${bkgpanel[bkgcount]} h-100">
+          <div class="card-body">
+            <h5 class="card-title">${statusInfield}</h5>
+            <p class="card-text fs-3">${count}</p>
+          </div>
+          <div class="card-footer bg-transparent border-top-0">
+            <a href="list.html?status=${encodeURIComponent(statuInfields)}" class="btn btn-light">View ${statusInfield}</a>
+          </div>
+        </div>
+      `;
+
+      panelContainerInfield.appendChild(col);
       // increment bkg count to move through the array
       bkgcount++;
     } catch (err) {
